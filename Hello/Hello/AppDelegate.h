6aac1962c4525a0a98d7fbed4accd9daf8de1018
xwhy <xwhy@xuyangdeMBP.lan>
2015-06-14 23:51:33 +0800

    Initial Commit

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by xwhy on 15/6/14.
+//  Copyright (c) 2015年 xwhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by xwhy on 15/6/14.
+//  Copyright (c) 2015年 xwhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
